fbc69fdd1afd99913ef3562e27951e0e22504ae2
sun-diane Sun <dianesun1@gmail.com>
2023-04-17 22:33:44 +0000

    Second commit

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -1,19 +1,23 @@
-class CurrencyController < ApplicationController 
-
+class CurrencyController < ApplicationController
   def get_currency_one
+    render({ :template => "/currency/get_currency_one.html.erb" })
+  end
+
+  def get_currency_two
+    @original_currency = params[:currency_one]
+
+    render({ :template => "/currency/get_currency_two.html.erb" })
+  end
+
+  def convert
+    @original_currency = params[:currency_one]
+    @new_currency = params[:currency_two]
+
     url = "https://api.exchangerate.host/latest"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     @response_obj = JSON.parse(response)
 
-    render({ :template => "/currency/get_currency_one.html.erb" })
-  end
-
-  def get_currency_two
-    render({ :template => "/currency/get_currency_two.html.erb" })
-  end
-
-  def convert
     render({ :template => "/currency/convert.html.erb" })
   end
 end
